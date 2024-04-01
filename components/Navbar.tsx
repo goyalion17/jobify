68832053d64830e55ea53a8129e5d240120d73df
Irina Saveleva <goyalion17@gmail.com>
2024-04-01 16:22:09 +0200

    building navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,17 @@
+import { UserButton } from "@clerk/nextjs";
+import LinksDropdown from "./LinksDropdown";
+import ThemeToggle from "./ThemeToggle";
+
 const Navbar = () => {
-  return <h2 className="text-4xl">Navbar</h2>;
+  return (
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
+      <LinksDropdown />
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
