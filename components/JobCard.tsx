bd90c4f7d27bdef06aaae9ace2004afade4c8794
Irina Saveleva <goyalion17@gmail.com>
2024-04-18 19:52:02 +0200

    building job card

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,7 +1,38 @@
+import { JobType } from "@/utils/types";
+import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
 
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import JobInfo from "./JobInfo";
+import DeleteJobButton from "./DeleteJobButton";
 
-function JobCard() {
-  return <h1 className="text-4xl">JobCard</h1>;
+function JobCard({ job }: { job: JobType }) {
+  return (
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent></CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size="sm">
+          <Link href={`/${job.id}`}>edit</Link>
+        </Button>
+        <DeleteJobButton />
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default JobCard;
