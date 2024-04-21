2eb19e288d754a3f114aa771e6dc72813211f0e2
Irina Saveleva <goyalion17@gmail.com>
2024-04-21 15:15:58 +0200

    building stats action

--- app/(dashboard)/stats/page.tsx
+++ app/(dashboard)/stats/page.tsx
@@ -1,4 +1,8 @@
-const StatsPage = () => {
+import { getStatsAction } from "@/utils/actions";
+
+const StatsPage = async () => {
+  const stats = await getStatsAction();
+  console.log(stats);
   return <h1 className="text-4xl">StatsPage</h1>;
 };
 
